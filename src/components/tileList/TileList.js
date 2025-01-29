db13d7de39615d7f52aca64242928095598cf274
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2025-01-29 21:40:37 +0000

    removal of console.log for testing if list gets passed to component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Tile } from '../tile/Tile'
+import { Tile } from '../tile/Tile.js'
 
 export const TileList = (props) => {
   return (
     <div className='tile-list'>
       {props.list.map(({name, ...rest}) => {
-        <Tile key={name} name={name} description={rest}/>
+        return <Tile key={name} name={name} description={rest}/>
       })}
     </div>
   );
