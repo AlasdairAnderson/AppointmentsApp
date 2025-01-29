583d1059c3aaa640cd74c15e0f7464973e04a33b
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2025-01-29 18:35:14 +0000

    additon of name, phone, email, duplicateName variables to track state of contacts. Handle the submission of a form and identify if name is already within contacts list.

---
+++ src/containers/contactsPage/ContactsPage.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+
+import { ContactForm } from "../../components/contactForm/ContactForm";
+import { TileList } from "../../components/tileList/TileList";
+
+export const ContactsPage = (props) => {
+  /*
+  Define state variables for 
+  contact info and duplicate check
+  */
+  const contacts = props.contacts;
+  const addContact = props.addContact;
+  
+  // Track contact info of form values
+  const [ name, setName ] = useState('');
+  const [ phone, setPhone ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ duplicateName, setDuplicateName] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*
+    Add contact info and clear data
+    if the contact name is not a duplicate
+    */
+    if(duplicateName){
+      addContact(name, phone, email);
+      document.getElementById("contact-form").reset(); 
+    } else {
+      alert('Name is already registered as a contact please choose another');
+    }
+  };
+
+  /*
+  Using hooks, check for contact name in the 
+  contacts array variable in props
+  */
+  useEffect(() => {
+    if(contacts.include(name)){
+      setDuplicateName(true);
+    } else {
+      setDuplicateName(false);
+    }
+  }, [name])
+
+  return (
+    <div>
+      <section>
+        <h2>Add Contact</h2> 
+        <ContactForm name={name} setName={setName} phone={phone} setPhone={setPhone} email={email} setEmail={setEmail} handleSubmit={handleSubmit}/>
+      </section>
+      <hr />
+      <section>
+        <h2>Contacts</h2>
+        <TileList contacts={contacts}/>
+      </section>
+    </div>
+  );
+};
